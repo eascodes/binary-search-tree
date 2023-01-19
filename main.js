21ada0c6b22f0e26948312f7a73aa40d105bbeb4
Emily <easchoettler@gmail.com>
2023-01-19 16:24:32 -0500

    Add buildTree functionality

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-const NodeFactory = (data) => {
+const NodeFactory = (data, left, right) => {
+    if (!left) left = null;
+    if (!right) right = null;
     data = data;
-    return { data }
+    left = left;
+    right = right;
+    return { data, left, right }
 }
 
 const TreeFactory = (arr) => {
@@ -26,10 +30,40 @@ function compareNumbers(a, b) {
   }
 
 function buildTree(arr) {
+    if (arr.length === 1) {
+        let root = NodeFactory(arr[0]);
+        return root;
+    };
+    if (arr.length < 1 || arr === null) return null;
+    
+    // Make copy of array, filter out duplicates, & sort
     let copy = [...arr];
     copy = removeDups(copy);
     copy = copy.sort(compareNumbers);
     
+    let mid = parseInt((copy.length - 1)/2);
+    let rootValue = copy[mid];
+    
+    let leftArr;
+    let rightArr;
+    if (copy.slice(0,mid)) {
+        leftArr = copy.slice(0,mid);
+    } else {
+        leftArr = null;
+    }
+    
+    if (copy.slice((mid+1),copy.length)) {
+        rightArr = copy.slice((mid+1),copy.length);
+    } else {
+        rightArr = null;
+    }
+
+    let root = NodeFactory(rootValue);
+    root.left = buildTree(leftArr);
+    root.right = buildTree(rightArr);
+
+    return root;
+
 }
 
 const prettyPrint = (node, prefix = '', isLeft = true) => {
